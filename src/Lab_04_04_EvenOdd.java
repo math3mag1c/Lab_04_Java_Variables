00f895b539b3b3a8de9f671a1461d94422ff84eb
math3mag1c <140988106+math3mag1c@users.noreply.github.com>
2025-08-30 17:30:04 -0400

    Completed Task 4 in Part 2

---
+++ src/Lab_04_04_EvenOdd.java
@@ -0,0 +1,11 @@
+public class Lab_04_04_EvenOdd {
+    public static void main(String[] args) {
+        int numToExamine = 240349857;
+        if (numToExamine % 2 == 1) {
+            System.out.println("The number you entered is odd");
+        }
+        else {
+            System.out.println("The number you entered is even");
+        }
+    }
+}
