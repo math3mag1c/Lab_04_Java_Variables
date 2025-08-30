97a2bd91feb25587d4a75c5c9c7160fbc6169467
math3mag1c <140988106+math3mag1c@users.noreply.github.com>
2025-08-30 17:23:36 -0400

    Completed Task 2 in Part 2

---
+++ src/Lab_04_02_YearlyMaintenanceCost.java
@@ -0,0 +1,14 @@
+public class Lab_04_02_YearlyMaintenanceCost {
+    public static void main(String[] args) {
+        double springCost = 120.24;
+        double summerCost = 3458.35;
+        double fallCost = 9874.25;
+        double winterCost = 398476.35;
+        double totalYearlyCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("The spring cost is " + springCost);
+        System.out.println("The summer cost is " + summerCost);
+        System.out.println("The fall cost is " + fallCost);
+        System.out.println("The winter cost is " + winterCost);
+        System.out.println("The total yearly cost is " + totalYearlyCost + ".");
+    }
+}
